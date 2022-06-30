72c6c42a1854df66236c06ca586ef46a0ab564d7
Ndu ifeanyi <nduros1@gmail.com>
2022-06-30 03:21:46 -0700

    removed .next build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0e4a7e4d43c37904"; }
+/******/ 		__webpack_require__.h = function() { return "6d88635f2df8151b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
